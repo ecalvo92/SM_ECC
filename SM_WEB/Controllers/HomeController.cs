c614bd959447897ccf863bcadf09dbfcb3afd35e
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-01-31 08:59:30 -0600

    Creación de vista de Login

--- SM_WEB/Controllers/HomeController.cs
+++ SM_WEB/Controllers/HomeController.cs
@@ -1,25 +1,20 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using SM_WEB.Models;
 
 namespace SM_WEB.Controllers
 {
     public class HomeController : Controller
     {
+        //Registro, Recuperar Contraseña
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Login()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
