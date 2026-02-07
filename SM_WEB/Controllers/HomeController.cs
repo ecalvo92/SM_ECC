ed2dc919029f51befefd4abe88f2fa21bc41888d
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-02-07 08:42:48 -0600

    Diseño de Crear Cuenta y Recuperar Acceso

--- SM_WEB/Controllers/HomeController.cs
+++ SM_WEB/Controllers/HomeController.cs
@@ -5,13 +5,13 @@ namespace SM_WEB.Controllers
 {
     public class HomeController : Controller
     {
-        //Registro, Recuperar Contraseña
-
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        #region Iniciar Sesión
 
         [HttpGet]
         public IActionResult Login()
@@ -24,6 +24,40 @@ namespace SM_WEB.Controllers
         {
             return View();
         }
+
+        #endregion
+
+        #region Registrar Cuenta
+
+        [HttpGet]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registro(Usuario modelo)
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Recuperar Acceso
+
+        [HttpGet]
+        public IActionResult RecuperarAcceso()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RecuperarAcceso(Usuario modelo)
+        {
+            return View();
+        }
+
+        #endregion
 
     }
 }
